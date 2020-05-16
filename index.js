8d2a97e2dd610d4bc3364d0a0b7f0c3993c3a595
Revan Sarbast <revansarbast8@gmail.com>
2020-05-16 01:11:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let right = parseInt(rightNumbers, 10);
  
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+  if (right > 0) {
+    dodger.style.left = `${right - 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  }
+  else if(e.key === "ArrowRight"){
+    moveDodgerRight();
   }
 });
